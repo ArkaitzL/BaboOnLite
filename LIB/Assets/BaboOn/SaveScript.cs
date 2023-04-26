36dd18db6bdc937562391d1fd6ca88cbf83cd92e
Arkaitz leon <arkaitz.leon@ludusglobal.com>
2023-04-26 15:00:26 +0200

    save acabado

--- LIB/Assets/BaboOn/SaveScript.cs
+++ LIB/Assets/BaboOn/SaveScript.cs
@@ -4,6 +4,9 @@ namespace baboOn
     [System.Serializable]
     public class SaveScript
     {
-        public string var = "Ejemplo: esta es una variable de ejemplo";
+        //----------------------------------------------------------------//
+        //public string var = "Ejemplo: esta es una variable de ejemplo"; //
+        //----------------------------------------------------------------//
+
     }
 }
